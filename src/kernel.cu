43a758c2cf9fa752ba52495d9f1de052e368c2c1
Edward Atter <ed@gocybered.com>
2018-09-07 22:57:11 -0400

    Brute force simulation working, need to implement max velocity

--- src/kernel.cu
+++ src/kernel.cu
@@ -230,10 +230,43 @@ void Boids::copyBoidsToVBO(float *vbodptr_positions, float *vbodptr_velocities) 
 * in the `pos` and `vel` arrays.
 */
 __device__ glm::vec3 computeVelocityChange(int N, int iSelf, const glm::vec3 *pos, const glm::vec3 *vel) {
-  // Rule 1: boids fly towards their local perceived center of mass, which excludes themselves
-  // Rule 2: boids try to stay a distance d away from each other
-  // Rule 3: boids try to match the speed of surrounding boids
-  return glm::vec3(0.0f, 0.0f, 0.0f);
+    // Rule 1: boids fly towards their local perceived center of mass, which excludes themselves
+    glm::vec3 currentPose = pos[iSelf];
+    glm::vec3 center (0.0f, 0.0f, 0.0f);
+    glm::vec3 c (0.0f, 0.0f, 0.0f);
+    glm::vec3 calcVel (0.0f, 0.0f, 0.0f);
+
+    for (int i = 0; i < N; i++){
+        if (iSelf == i) { continue; }
+        //get distance
+        float d = glm::distance(currentPose, pos[i]);
+        if (d > rule1Distance) { continue; }
+        center += pos[i];
+    }
+    //center /= 1;// TODO N - 1;
+    center -= currentPose;
+    center *= rule1Scale;
+
+    // Rule 2: boids try to stay a distance d away from each other
+    for (int i = 0; i < N; i++) {
+        if (iSelf != i) { continue; }
+        float d = glm::distance(currentPose, pos[i]);
+        if (d > rule2Distance) { continue; }
+        c -= (pos[i] - currentPose);
+    }
+    c *= rule2Scale;
+
+    // Rule 3: boids try to match the speed of surrounding boid
+    for (int i = 0; i < N; i++) {
+        if (iSelf != i) { continue; }
+        float d = glm::distance(currentPose, pos[i]);
+        if (d > rule3Distance) { continue; }
+        calcVel += (vel[i]);
+    }
+    //calcVel /= 1;//TODO N - 1;
+    calcVel *= rule3Scale;
+
+    return center + c + calcVel;
 }
 
 /**
@@ -245,6 +278,13 @@ __global__ void kernUpdateVelocityBruteForce(int N, glm::vec3 *pos,
   // Compute a new velocity based on pos and vel1
   // Clamp the speed
   // Record the new velocity into vel2. Question: why NOT vel1?
+  int i = threadIdx.x + (blockIdx.x * blockDim.x);
+
+  if (i >= N) { return; }
+
+  glm::vec3 newVector = computeVelocityChange(N, i, pos, vel1) + vel1[i];
+  //TODO Limit max speed
+  vel2[i] = newVector;
 }
 
 /**
@@ -349,6 +389,15 @@ __global__ void kernUpdateVelNeighborSearchCoherent(
 void Boids::stepSimulationNaive(float dt) {
   // TODO-1.2 - use the kernels you wrote to step the simulation forward in time.
   // TODO-1.2 ping-pong the velocity buffers
+	int blocksPerGrid = (numObjects + blockSize - 1) / blockSize;
+
+	kernUpdateVelocityBruteForce <<< blocksPerGrid, blockSize >>>(numObjects, dev_pos, dev_vel1, dev_vel2);
+	checkCUDAErrorWithLine("kernUpdateVelocityBruteForce ::");
+
+	kernUpdatePos <<< blocksPerGrid, blockSize >>> (numObjects, dt, dev_pos, dev_vel1);
+	checkCUDAErrorWithLine("kernUpdatePos ::");
+
+	std::swap(dev_vel1, dev_vel2);
 }
 
 void Boids::stepSimulationScatteredGrid(float dt) {
